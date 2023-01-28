ffdf41162d4ee739bb57b205e816ee6f6ec77ec5
Insaneboy9 <2717941S@student.gla.ac.uk>
2023-01-28 20:18:20 +0800

    added empty homescreen

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,22 +1,26 @@
-import React from 'react';
+import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
 import LoginScreen from "../screens/login/LoginScreen";
+import HomeScreen from "../screens/home/HomeScreen";
 
 const Stack = createNativeStackNavigator();
 
 export default function AppNavigator() {
   return (
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen
-            name="LoginScreen"
-            component={LoginScreen}
-            options={{ headerShown: false }}
-          ></Stack.Screen>
-        </Stack.Navigator>
-      </NavigationContainer>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="LoginScreen"
+          component={LoginScreen}
+          options={{ headerShown: false }}
+        ></Stack.Screen>
+        <Stack.Screen
+          name="HomeScreen"
+          component={HomeScreen}
+          options={{ headerShown: false }}
+        ></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-
---
+++ screens/home/HomeScreen.js
@@ -0,0 +1,12 @@
+import { SafeAreaView, Text } from 'react-native'
+import React from 'react'
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>HomeScreen</Text>
+    </SafeAreaView>
+  )
+}
+
+export default HomeScreen
