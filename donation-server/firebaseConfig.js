5ab2e21cd5875cc30b970890faeeb1f5ece71c16
Insaneboy9 <daryl_seah89@hotmail.com>
2023-02-16 00:29:44 +0800

    Sucessfuly done the backend for retrieving hawkers

--- donation-server/firebaseConfig.js
+++ donation-server/firebaseConfig.js
@@ -1,5 +1,7 @@
 // Import the functions you need from the SDKs you need
-const firebase= require("firebase")
+import { initializeApp } from "firebase/app"
+import { getFirestore} from "firebase/firestore"
+import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -14,6 +16,9 @@ const firebaseConfig = {
     measurementId: "G-BZSV92Z7QW"
 };
 
-firebase.initializeApp(firebaseConfig)
-const db = firebase.firestore();
-module.exports = db;
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+const storage = getStorage(app);
+
+
+export {db, storage};
--- donation-server/index.js
+++ donation-server/index.js
@@ -1,8 +1,41 @@
-const express = require('express')
-const cors = require('cors')
-const db = require('./firebaseConfig')
+
+import express from "express";
+import cors from "cors";
+import {db, storage} from "./firebaseConfig.js";
+import { collection, getDocs } from "firebase/firestore";
+import {ref, getDownloadURL} from "firebase/storage";
+
+
 const app = express()
 app.use(express.json())
 app.use(cors())
 
 app.listen(8080, () => console.log("Up and Running 8080"))
+
+// app.get("/hawkers", async (req,res) => {
+//     const snapshot = await getDocs(collection(db, "hawkers"));
+//     const list = snapshot.docs.map((doc) => ({id: doc.id, ...doc.data()}))
+//     res.send(list);
+// });
+
+app.get("/hawkers", async (req,res) => {
+    const snapshot = await getDocs(collection(db, "hawkers"));
+    const list = snapshot.docs.map(async (doc) => {
+        const data = doc.data();
+        const posterUrl = await getDownloadURL(ref(storage, data.poster)); // get poster URL
+        const thumbnailUrl = await getDownloadURL(ref(storage, data.thumbnail)); // get thumbnail URL
+        return {
+          id: doc.id,
+          ...data,
+          posterUrl,
+          thumbnailUrl
+        };
+      });
+      const results = await Promise.all(list); // wait for all the URLs to resolve
+      res.send(results);
+});
+
+app.get("/", async (req,res) => {
+    console.log("hi")
+    res.send(200);
+});
